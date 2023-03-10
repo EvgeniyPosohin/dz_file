9c448c94eb40fc32f7e0ea498e89be3e37444a4a
EvgeniyPosohin <mad.house@bk.ru>
2023-03-10 16:53:59 +0500

    pravka-2

--- dz_3_file.py
+++ dz_3_file.py
@@ -1,22 +1,19 @@
 import os
 
 list_file = ['1.txt', '2.txt', '3.txt']
-list_read = []
 name_txt = {}
 for i in range(len(list_file)):
     full_path = os.path.join(os.getcwd(), list_file[i])
     with open(full_path, 'r', encoding='utf-8') as file:
-        list_read.append(file.readlines())
-        name_txt[len(list_read[i])] = list_file[i]
-list_read.sort()
-list_read.reverse()
-print(list_read)
-print(name_txt)
+        x = file.readlines()
+        name_txt[len(x)] = [list_file[i], x]
+print(sorted(name_txt))
+
 with open('dz_3.txt', 'w') as file:
-    for id, i in enumerate(sorted(name_txt)):
-        file.write(name_txt[i]+'\n')
+    for i in sorted(name_txt):
+        file.write(name_txt[i][0]+'\n')
         file.write(str(i)+'\n')
-        file.write(''.join(list_read[id])+'\n')
+        file.write(''.join(name_txt[i][1])+'\n')
 
 
 
